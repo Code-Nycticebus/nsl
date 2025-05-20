5d71b06e38f0db87aef2372d239a0e305f6fa532
Code-Nycticebus <loris.kriyonas@gmail.com>
2025-05-20 23:19:09 +0200

    create ignored build directory

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 #!/bin/python3
 import subprocess
+from pathlib import Path
 
 
 if __name__ == "__main__":
+    build = Path("build")
+    build.mkdir(exist_ok=True)
+    
+    Path(build, ".gitignore").write_text("*")
+
     subprocess.run(
         ["gcc", "-Werror", "-Wall", "-Wextra", "-Iinclude", "tests/main.c", "-o", "build/test"]
     )
