2fb67ecbb94fbb4e4d63fe185ede4c68a7b8bf8c
Code-Nycticebus <loris.kriyonas@gmail.com>
2025-05-23 18:39:28 +0200

    added a NC_ASSERT macro

--- include/nc/defines.h
+++ include/nc/defines.h
@@ -20,13 +20,19 @@
     typedef double    f64;
 #endif
 
+#ifndef NC_ASSERT
+#    include <assert.h>
+#    define NC_ASSERT assert
+#endif
+
 #define BOOL_FMT "%s"
 #define BOOL_ARG(b) (b ? "true" : "false")
 
 #define NC_ARRAY_LEN(...) (sizeof(__VA_ARGS__) / sizeof((__VA_ARGS__)[0]))
 
 #define NC_STR(str) ((nc_Str){.len = sizeof(str) - 1, .data = (str)})
-#define NC_STR_STATIC(str) {.len = sizeof(str) - 1, .data = (str)}
+#define NC_STR_STATIC(str)                                                                         \
+    { .len = sizeof(str) - 1, .data = (str) }
 #define NC_STR_FMT "%.*s"
 #define NC_STR_REPR "'%.*s'"
 #define NC_STR_ARG(str) (i32)(str).len, (str).data
@@ -36,87 +42,92 @@ typedef struct {
     const char *data;
 } nc_Str;
 
-#define NC_BYTES(...) (nc_Bytes){sizeof((const u8[]){__VA_ARGS__}), (const u8[]){ __VA_ARGS__ }}
-#define NC_BYTES_STR(s) (nc_Bytes){ sizeof(s) - 1, (const u8 *)(s)}
+#define NC_BYTES(...)                                                                              \
+    (nc_Bytes) {                                                                                   \
+        .size = sizeof((const u8[]){__VA_ARGS__}), .data = (const u8[]){__VA_ARGS__},              \
+    }
+
+#define NC_BYTES_STR(s)                                                                            \
+    (nc_Bytes) {                                                                                   \
+        .size = sizeof(s) - 1, .data = (const u8 *)(s),                                            \
+    }
 
 typedef struct {
-  usize size;
-  const u8 *data;
+    usize size;
+    const u8 *data;
 } nc_Bytes;
 
 #if defined(__clang__)
-    #define NC_COMPILER_CLANG 1
-    #define NC_COMPILER_NAME "clang"
+#    define NC_COMPILER_CLANG 1
+#    define NC_COMPILER_NAME "clang"
 #elif defined(__GNUC__) && !defined(__clang__)
-    #define NC_COMPILER_GCC 1
-    #define NC_COMPILER_NAME "gcc"
+#    define NC_COMPILER_GCC 1
+#    define NC_COMPILER_NAME "gcc"
 #elif defined(__TINYC__)
-    #define NC_COMPILER_TCC 1
-    #define NC_COMPILER_NAME "tcc"
+#    define NC_COMPILER_TCC 1
+#    define NC_COMPILER_NAME "tcc"
 #elif defined(_MSC_VER)
-    #define NC_COMPILER_MSVC 1
-    #define NC_COMPILER_NAME "cl"
+#    define NC_COMPILER_MSVC 1
+#    define NC_COMPILER_NAME "cl"
 #elif defined(__MINGW32__)
-    #define NC_COMPILER_MINGW32 1
-    #define NC_COMPILER_NAME "mingw32"
+#    define NC_COMPILER_MINGW32 1
+#    define NC_COMPILER_NAME "mingw32"
 #elif defined(__MINGW64__)
-    #define NC_COMPILER_MINGW64 1
-    #define NC_COMPILER_NAME "mingw64"
+#    define NC_COMPILER_MINGW64 1
+#    define NC_COMPILER_NAME "mingw64"
 #else
-    #define NC_COMPILER_UNKNOWN 1
-    #define NC_COMPILER_NAME "unknown"
+#    define NC_COMPILER_UNKNOWN 1
+#    define NC_COMPILER_NAME "unknown"
 #endif
 
 #if defined(NC_COMPILER_GCC) || defined(NC_COMPILER_CLANG)
-    #define NC_EXPORT            __attribute__((used))
-    #define NC_NORETURN          __attribute__((noreturn))
-    #define NC_PURE_FN           __attribute__((pure)) __attribute__((warn_unused_result))
-    #define NC_CONST_FN          __attribute__((const)) __attribute__((warn_unused_result))
-    #define NC_LIKELY(exp)       __builtin_expect(!!(exp), 1)
-    #define NC_UNLIKELY(exp)     __builtin_expect(!!(exp), 0)
-    #define NC_FMT(fmt_idx)      __attribute__((format(printf, fmt_idx, fmt_idx + 1)))
+#    define NC_EXPORT __attribute__((used))
+#    define NC_NORETURN __attribute__((noreturn))
+#    define NC_PURE_FN __attribute__((pure)) __attribute__((warn_unused_result))
+#    define NC_CONST_FN __attribute__((const)) __attribute__((warn_unused_result))
+#    define NC_LIKELY(exp) __builtin_expect(!!(exp), 1)
+#    define NC_UNLIKELY(exp) __builtin_expect(!!(exp), 0)
+#    define NC_FMT(fmt_idx) __attribute__((format(printf, fmt_idx, fmt_idx + 1)))
 #elif defined(NC_COMPILER_MSVC)
-    #include <sal.h>
-    #define NC_EXPORT            __declspec(dllexport)
-    #define NC_NORETURN          __declspec(noreturn)
-    #define NC_PURE_FN           _Check_return_
-    #define NC_CONST_FN          _Check_return_
-    #define NC_LIKELY(exp)       (exp)
-    #define NC_UNLIKELY(exp)     (exp)
-    #define NC_FMT(fmt_idx)
+#    include <sal.h>
+#    define NC_EXPORT __declspec(dllexport)
+#    define NC_NORETURN __declspec(noreturn)
+#    define NC_PURE_FN _Check_return_
+#    define NC_CONST_FN _Check_return_
+#    define NC_LIKELY(exp) (exp)
+#    define NC_UNLIKELY(exp) (exp)
+#    define NC_FMT(fmt_idx)
 #else
-    #define NC_EXPORT
-    #define NC_NORETURN
-    #define NC_UNUSED
-    #define NC_PURE_FN
-    #define NC_CONST_FN
-    #define NC_LIKELY(exp)       (exp)
-    #define NC_UNLIKELY(exp)     (exp)
-    #define NC_FMT(fmt_idx)
+#    define NC_EXPORT
+#    define NC_NORETURN
+#    define NC_UNUSED
+#    define NC_PURE_FN
+#    define NC_CONST_FN
+#    define NC_LIKELY(exp) (exp)
+#    define NC_UNLIKELY(exp) (exp)
+#    define NC_FMT(fmt_idx)
 #endif
-
 
 #ifdef NC_STATIC
-    #define nc_api static
+#    define nc_api static
 #elif defined(_WIN32)
-    #ifdef NC_BUILD
-        #define nc_api __declspec(dllexport)
-    #else
-        #define nc_api __declspec(dllimport)
-    #endif
+#    ifdef NC_BUILD
+#        define nc_api __declspec(dllexport)
+#    else
+#        define nc_api __declspec(dllimport)
+#    endif
 #else
-    #define nc_api __attribute__((visibility("default")))
+#    define nc_api __attribute__((visibility("default")))
 #endif
 
-
 #if defined(__BYTE_ORDER__) && defined(__ORDER_BIG_ENDIAN__) && defined(__ORDER_LITTLE_ENDIAN__)
-    #define NC_ENDIAN_LITTLE __ORDER_LITTLE_ENDIAN__
-    #define NC_ENDIAN_BIG __ORDER_BIG_ENDIAN__
-    #define NC_BYTE_ORDER __BYTE_ORDER__
+#    define NC_ENDIAN_LITTLE __ORDER_LITTLE_ENDIAN__
+#    define NC_ENDIAN_BIG __ORDER_BIG_ENDIAN__
+#    define NC_BYTE_ORDER __BYTE_ORDER__
 #elif defined(_MSC_VER)
-    #define NC_ENDIAN_LITTLE 1234
-    #define NC_ENDIAN_BIG 4321
-    #define NC_BYTE_ORDER ENDIAN_LITTLE
+#    define NC_ENDIAN_LITTLE 1234
+#    define NC_ENDIAN_BIG 4321
+#    define NC_BYTE_ORDER ENDIAN_LITTLE
 #else
-    #error "No Byte Order detected!"
+#    error "No Byte Order detected!"
 #endif
