0b25c6fcdfbc48c74a70289d7dd0155c9d1a430f
Code-Nycticebus <loris.kriyonas@gmail.com>
2025-07-06 18:58:27 +0200

    formatted int.h

--- nsl.h
+++ nsl.h
@@ -348,35 +348,35 @@ NSL_API nsl_Path nsl_path_parent(nsl_Path path);
 
 
 #define INTEGER_DECL(T)                                                                            \
-    NSL_API NSL_CONST_FN T nsl_##T##_reverse_bits(T value);                                           \
-    NSL_API NSL_CONST_FN usize nsl_##T##_leading_ones(T value);                                       \
-    NSL_API NSL_CONST_FN usize nsl_##T##_trailing_ones(T value);                                      \
-    NSL_API NSL_CONST_FN usize nsl_##T##_leading_zeros(T value);                                      \
-    NSL_API NSL_CONST_FN usize nsl_##T##_trailing_zeros(T value);                                     \
-    NSL_API NSL_CONST_FN usize nsl_##T##_count_zeros(T value);                                        \
-    NSL_API NSL_CONST_FN usize nsl_##T##_count_ones(T value);                                         \
+    NSL_API NSL_CONST_FN T nsl_##T##_reverse_bits(T value);                                        \
+    NSL_API NSL_CONST_FN usize nsl_##T##_leading_ones(T value);                                    \
+    NSL_API NSL_CONST_FN usize nsl_##T##_trailing_ones(T value);                                   \
+    NSL_API NSL_CONST_FN usize nsl_##T##_leading_zeros(T value);                                   \
+    NSL_API NSL_CONST_FN usize nsl_##T##_trailing_zeros(T value);                                  \
+    NSL_API NSL_CONST_FN usize nsl_##T##_count_zeros(T value);                                     \
+    NSL_API NSL_CONST_FN usize nsl_##T##_count_ones(T value);                                      \
                                                                                                    \
-    NSL_API NSL_CONST_FN T nsl_##T##_swap_bytes(T value);                                             \
+    NSL_API NSL_CONST_FN T nsl_##T##_swap_bytes(T value);                                          \
                                                                                                    \
-    NSL_API NSL_CONST_FN T nsl_##T##_to_be(T value);                                                  \
-    NSL_API NSL_CONST_FN T nsl_##T##_from_be(T value);                                                \
-    NSL_API T nsl_##T##_from_be_bytes(nsl_Bytes bytes);                                               \
-    NSL_API nsl_Bytes nsl_##T##_to_be_bytes(T value, nsl_Arena *arena);                                \
+    NSL_API NSL_CONST_FN T nsl_##T##_to_be(T value);                                               \
+    NSL_API NSL_CONST_FN T nsl_##T##_from_be(T value);                                             \
+    NSL_API T nsl_##T##_from_be_bytes(nsl_Bytes bytes);                                            \
+    NSL_API nsl_Bytes nsl_##T##_to_be_bytes(T value, nsl_Arena *arena);                            \
                                                                                                    \
-    NSL_API NSL_CONST_FN T nsl_##T##_to_le(T value);                                                  \
-    NSL_API NSL_CONST_FN T nsl_##T##_from_le(T value);                                                \
-    NSL_API T nsl_##T##_from_le_bytes(nsl_Bytes bytes);                                               \
-    NSL_API nsl_Bytes nsl_##T##_to_le_bytes(T value, nsl_Arena *arena);                                \
+    NSL_API NSL_CONST_FN T nsl_##T##_to_le(T value);                                               \
+    NSL_API NSL_CONST_FN T nsl_##T##_from_le(T value);                                             \
+    NSL_API T nsl_##T##_from_le_bytes(nsl_Bytes bytes);                                            \
+    NSL_API nsl_Bytes nsl_##T##_to_le_bytes(T value, nsl_Arena *arena);                            \
                                                                                                    \
-    NSL_API T nsl_##T##_from_ne_bytes(nsl_Bytes bytes);                                               \
-    NSL_API nsl_Bytes nsl_##T##_to_ne_bytes(T value, nsl_Arena *arena);                                \
+    NSL_API T nsl_##T##_from_ne_bytes(nsl_Bytes bytes);                                            \
+    NSL_API nsl_Bytes nsl_##T##_to_ne_bytes(T value, nsl_Arena *arena);                            \
                                                                                                    \
-    NSL_API NSL_CONST_FN T nsl_##T##_max(T a, T b);                                                   \
-    NSL_API NSL_CONST_FN T nsl_##T##_min(T a, T b);                                                   \
-    NSL_API NSL_CONST_FN T nsl_##T##_clamp(T min, T max, T value);                                    \
+    NSL_API NSL_CONST_FN T nsl_##T##_max(T a, T b);                                                \
+    NSL_API NSL_CONST_FN T nsl_##T##_min(T a, T b);                                                \
+    NSL_API NSL_CONST_FN T nsl_##T##_clamp(T min, T max, T value);                                 \
                                                                                                    \
-    NSL_API NSL_CONST_FN u64 nsl_##T##_hash(T value);                                                 \
-    NSL_API void nsl_##T##_swap(T *v1, T *v2);                                                       \
+    NSL_API NSL_CONST_FN u64 nsl_##T##_hash(T value);                                              \
+    NSL_API void nsl_##T##_swap(T *v1, T *v2);                                                     \
                                                                                                    \
     NSL_API NSL_CONST_FN T nsl_##T##_next_pow2(T n);
 
