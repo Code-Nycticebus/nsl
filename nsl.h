cf13bb23a55902d3b64fa83568d0813e4e02306f
Code-Nycticebus <loris.kriyonas@gmail.com>
2025-07-06 11:15:03 +0200

    cleaned up defines.h

--- nsl.h
+++ nsl.h
@@ -66,7 +66,8 @@ int main(int argc, const char** argv) {
 
 #define NSL_STR(str) ((nsl_Str){.len = sizeof(str) - 1, .data = (str)})
 #define NSL_STR_STATIC(str) { .len = sizeof(str) - 1, .data = (str) }
-#define NSL_STR_FMT "%.*s"
+
+#define NSL_STR_FMT  "%.*s"
 #define NSL_STR_REPR "'%.*s'"
 #define NSL_STR_ARG(str) (i32)(str).len, (str).data
 
@@ -75,13 +76,13 @@ typedef struct {
     const char *data;
 } nsl_Str;
 
-#define NSL_BYTES(...)                                                                              \
-    (nsl_Bytes) {                                                                                   \
+#define NSL_BYTES(...)                                                                             \
+    (nsl_Bytes) {                                                                                  \
         .size = sizeof((const u8[]){__VA_ARGS__}), .data = (const u8[]){__VA_ARGS__},              \
     }
 
-#define NSL_BYTES_STR(s)                                                                            \
-    (nsl_Bytes) {                                                                                   \
+#define NSL_BYTES_STR(s)                                                                           \
+    (nsl_Bytes) {                                                                                  \
         .size = sizeof(s) - 1, .data = (const u8 *)(s),                                            \
     }
 
@@ -107,11 +108,11 @@ typedef struct {
     const char *message; // human-readable message
 } nsl_Error;
 
-#define nsl_List(T)                                                                                 \
+#define nsl_List(T)                                                                                \
     struct {                                                                                       \
         usize cap;                                                                                 \
         usize len;                                                                                 \
-        nsl_Arena *arena;                                                                           \
+        nsl_Arena *arena;                                                                          \
         T *items;                                                                                  \
     }
 
@@ -121,25 +122,27 @@ typedef nsl_List(u8) nsl_ByteBuffer;
 #define NSL_UNUSED(v) (void)(v)
 #define NSL_PASS ((void)(0))
 
-#define NSL_PANIC(msg)                                                                              \
+#define NSL_DEFER(v) do { result = (v); goto defer; } while(0)
+
+#define NSL_PANIC(msg)                                                                             \
     do {                                                                                           \
         fprintf(stderr, "Panic: %s:%d: %s\n", __FILE__, __LINE__, msg);                            \
         abort();                                                                                   \
     } while (0)
 
-#define NSL_UNREACHABLE(msg)                                                                        \
+#define NSL_UNREACHABLE(msg)                                                                       \
     do {                                                                                           \
         fprintf(stderr, "Unreachable: %s:%d: %s\n", __FILE__, __LINE__, msg);                      \
         abort();                                                                                   \
     } while (0)
 
-#define NSL_TODO(msg)                                                                               \
+#define NSL_TODO(msg)                                                                              \
     do {                                                                                           \
         fprintf(stderr, "TODO: %s:%d: %s\n", __FILE__, __LINE__, msg);                             \
         abort();                                                                                   \
     } while (0)
 
-#define NSL_NOT_IMPLEMENTED(msg)                                                                    \
+#define NSL_NOT_IMPLEMENTED(msg)                                                                   \
     do {                                                                                           \
         fprintf(stderr, "Not Implemented: %s:%d: %s\n", __FILE__, __LINE__, msg);                  \
         abort();                                                                                   \
@@ -147,43 +150,43 @@ typedef nsl_List(u8) nsl_ByteBuffer;
 
 
 #if defined(__clang__)
-#    define NSL_COMPILER_CLANG 1
+#    define NSL_COMPILER_CLANG
 #    define NSL_COMPILER_NAME "clang"
 #elif defined(__GNUC__) && !defined(__clang__)
-#    define NSL_COMPILER_GCC 1
+#    define NSL_COMPILER_GCC
 #    define NSL_COMPILER_NAME "gcc"
 #elif defined(__TINYC__)
-#    define NSL_COMPILER_TCC 1
+#    define NSL_COMPILER_TCC
 #    define NSL_COMPILER_NAME "tcc"
 #elif defined(_MSC_VER)
-#    define NSL_COMPILER_MSVC 1
+#    define NSL_COMPILER_MSVC
 #    define NSL_COMPILER_NAME "cl"
 #elif defined(__MINGW32__)
-#    define NSL_COMPILER_MINGW32 1
+#    define NSL_COMPILER_MINGW32
 #    define NSL_COMPILER_NAME "mingw32"
 #elif defined(__MINGW64__)
-#    define NSL_COMPILER_MINGW64 1
+#    define NSL_COMPILER_MINGW64
 #    define NSL_COMPILER_NAME "mingw64"
 #else
-#    define NSL_COMPILER_UNKNOWN 1
+#    define NSL_COMPILER_UNKNOWN
 #    define NSL_COMPILER_NAME "unknown"
 #endif
 
 #if defined(NSL_COMPILER_GCC) || defined(NSL_COMPILER_CLANG)
-#    define NSL_EXPORT __attribute__((used))
-#    define NSL_NORETURN __attribute__((noreturn))
-#    define NSL_PURE_FN __attribute__((pure)) __attribute__((warn_unused_result))
-#    define NSL_CONST_FN __attribute__((const)) __attribute__((warn_unused_result))
-#    define NSL_LIKELY(exp) __builtin_expect(!!(exp), 1)
+#    define NSL_EXPORT        __attribute__((used))
+#    define NSL_NORETURN      __attribute__((noreturn))
+#    define NSL_PURE_FN       __attribute__((pure)) __attribute__((warn_unused_result))
+#    define NSL_CONST_FN      __attribute__((const)) __attribute__((warn_unused_result))
+#    define NSL_LIKELY(exp)   __builtin_expect(!!(exp), 1)
 #    define NSL_UNLIKELY(exp) __builtin_expect(!!(exp), 0)
-#    define NSL_FMT(fmt_idx) __attribute__((format(printf, fmt_idx, fmt_idx + 1)))
+#    define NSL_FMT(fmt_idx)  __attribute__((format(printf, fmt_idx, fmt_idx + 1)))
 #elif defined(NSL_COMPILER_MSVC)
 #    include <sal.h>
-#    define NSL_EXPORT __declspec(dllexport)
-#    define NSL_NORETURN __declspec(noreturn)
-#    define NSL_PURE_FN _Check_return_
-#    define NSL_CONST_FN _Check_return_
-#    define NSL_LIKELY(exp) (exp)
+#    define NSL_EXPORT        __declspec(dllexport)
+#    define NSL_NORETURN      __declspec(noreturn)
+#    define NSL_PURE_FN       _Check_return_
+#    define NSL_CONST_FN      _Check_return_
+#    define NSL_LIKELY(exp)   (exp)
 #    define NSL_UNLIKELY(exp) (exp)
 #    define NSL_FMT(fmt_idx)
 #else
@@ -192,7 +195,7 @@ typedef nsl_List(u8) nsl_ByteBuffer;
 #    define NSL_UNUSED
 #    define NSL_PURE_FN
 #    define NSL_CONST_FN
-#    define NSL_LIKELY(exp) (exp)
+#    define NSL_LIKELY(exp)   (exp)
 #    define NSL_UNLIKELY(exp) (exp)
 #    define NSL_FMT(fmt_idx)
 #endif
@@ -200,9 +203,9 @@ typedef nsl_List(u8) nsl_ByteBuffer;
 #ifdef NSL_STATIC
 #    define NSL_API static
 #elif defined(_WIN32)
-#    ifdef NSL_BUILD
+#    if defined(NSL_DLL_BUILD)
 #        define NSL_API __declspec(dllexport)
-#    else
+#    elif defined(NSL_DLL_LOAD)
 #        define NSL_API __declspec(dllimport)
 #    endif
 #else
@@ -211,12 +214,12 @@ typedef nsl_List(u8) nsl_ByteBuffer;
 
 #if defined(__BYTE_ORDER__) && defined(__ORDER_BIG_ENDIAN__) && defined(__ORDER_LITTLE_ENDIAN__)
 #    define NSL_ENDIAN_LITTLE __ORDER_LITTLE_ENDIAN__
-#    define NSL_ENDIAN_BIG __ORDER_BIG_ENDIAN__
-#    define NSL_BYTE_ORDER __BYTE_ORDER__
+#    define NSL_ENDIAN_BIG    __ORDER_BIG_ENDIAN__
+#    define NSL_BYTE_ORDER    __BYTE_ORDER__
 #elif defined(_MSC_VER)
 #    define NSL_ENDIAN_LITTLE 1234
-#    define NSL_ENDIAN_BIG 4321
-#    define NSL_BYTE_ORDER NSL_ENDIAN_LITTLE
+#    define NSL_ENDIAN_BIG    4321
+#    define NSL_BYTE_ORDER    NSL_ENDIAN_LITTLE
 #else
 #    error "No Byte Order detected!"
 #endif
