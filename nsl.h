6858b906bf9b727065e8772cf2098de635848baf
Code-Nycticebus <loris.kriyonas@gmail.com>
2025-07-13 22:46:56 +0200

    improved the README

--- nsl.h
+++ nsl.h
@@ -24,33 +24,31 @@ int main(void) {
 ```
 
 ## Errors
-I've decided that functions which can fail should return an error code.
-Since a non-zero error code is considered truthy, I'm using the same logic for boolean-returning functions. They return true on failure and false on success.
+Functions that can fail return a `0` or `NSL_NO_ERROR` on success and a non-zero (truthy) value on failure.
 This mirrors the Unix convention where a non-zero exit code indicates an error.
 
 ## Data Structures
 
-The `nsl_List`, `nsl_Map`, and `nsl_Set` types are provided as data structures. All of them are valid when zero-initialized and can be used without any setup:
+The `nsl_List`, `nsl_Map`, and `nsl_Set` data structures are valid when zero-initialized and can be used without any setup:
 ```c
 nsl_Map map = {0};
 nsl_map_insert_u64(hash, 69);
 nsl_map_free(&map);
 ```
 
-Alternatively, you can allocate all internal memory from a `nsl_Arena`.
+Or, you can allocate all internal memory from a `nsl_Arena`.
 ```c
 nsl_Map map = {.arena = &arena};
 nsl_map_insert_u64(hash, 69);
 nsl_arena_free(&arena); // frees everything in the arena, including the map
 ```
 
-Or, if you want to free just the map itself (even if it's arena-backed):
+This works too if you want to free just the map itself:
 ```c
 nsl_Map map = {.arena = &arena};
 nsl_map_insert_u64(hash, 69);
 nsl_map_free(&arena); // only removes the map, arena remains intact
 ```
-
 
 ## TODO
 Here's whats left to do:
