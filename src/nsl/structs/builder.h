0e5e5a6ba9fdf5fa9c8cd77eb37983baf4c5e18e
Code-Nycticebus <loris.kriyonas@gmail.com>
2025-10-15 18:12:24 +0200

    added a byte buffer to the builder

--- src/nsl/structs/builder.h
+++ src/nsl/structs/builder.h
@@ -6,11 +6,16 @@
 #define nsl_sb_push_c(sb, c) nsl_list_push(sb, c)
 #define nsl_sb_push_cstr(sb, cstr) nsl_list_extend(sb, strlen(cstr), cstr)
 #define nsl_sb_push_str(sb, str) nsl_list_extend(sb, (str).len, (str).data)
-#define nsl_sb_push_fmt(sb, ...) do {\
-    i32 size = snprintf(NULL, 0, __VA_ARGS__);\
-    nsl_list_reserve(sb, size + 1);\
-    snprintf(&(sb)->items[(sb)->len ? (sb)->len-1 : 0], size + 1, __VA_ARGS__);\
-    (sb)->len += size;\
-} while(0);
+#define nsl_sb_push_fmt(sb, ...)                                                                   \
+    do {                                                                                           \
+        i32 size = snprintf(NULL, 0, __VA_ARGS__);                                                 \
+        nsl_list_reserve(sb, size + 1);                                                            \
+        snprintf(&(sb)->items[(sb)->len ? (sb)->len - 1 : 0], size + 1, __VA_ARGS__);              \
+        (sb)->len += size;                                                                         \
+    } while (0)
+#define nsl_sb_to_str(sb) nsl_str_from_parts((sb)->len, (sb)->items)
+
+#define nsl_bb_push_bytes(bb, size, bytes) nsl_list_extend(bb, size, (const u8*)bytes)
+#define nsl_bb_to_bytes(bb) nsl_bytes_from_parts((bb)->len, (bb)->items)
 
 #endif // _NSL_BUILDER_H_
--- tests/structs/test-builder.c
+++ tests/structs/test-builder.c
@@ -2,17 +2,29 @@
 
 static void test_string_builder(void) {
     nsl_StrBuilder sb = {0};
-    nsl_sb_push_fmt(&sb, "%s", "what");
 
-    nsl_sb_push_c(&sb, '\0');
-    printf("%s\n", sb.items); fflush(stdout);
+    nsl_sb_push_fmt(&sb, "%s", "what");
+    nsl_Str s = nsl_sb_to_str(&sb);
+    NSL_ASSERT(nsl_str_eq(s, NSL_STR("what")));
 
     nsl_list_free(&sb);
 }
 
+static void test_bytes_builder(void) {
+    nsl_ByteBuffer bb = {0};
+
+    int data[4] = {1, 2, 3, 4};
+    nsl_bb_push_bytes(&bb, sizeof(data), data);
+
+    nsl_Bytes bytes = nsl_bb_to_bytes(&bb);
+    NSL_ASSERT(nsl_bytes_eq(bytes, nsl_bytes_from_parts(sizeof(data), data)));
+
+    nsl_list_free(&bb);
+}
 
 
 void run_test_builder(void);
 void run_test_builder(void) {
     test_string_builder();
+    test_bytes_builder();
 }
