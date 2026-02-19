db202636da5345983412c420ed0f44c8ee162e68
Code-Nycticebus <loris.kriyonas@gmail.com>
2026-02-19 16:28:00 +0100

    improved build.c

--- build.c
+++ build.c
@@ -10,37 +10,52 @@
 #   define CC NSL_COMPILER_NAME
 #endif
 
-static nsl_Error build_header(void) {
-    nsl_Error result = NSL_NO_ERROR;
-    nsl_Cmd cmd = {0};
-
-    nsl_cmd_push(&cmd, CC, "-c", "-o", "nsl.o", "-DNSL_IMPLEMENTATION", "nsl.h");
-    nsl_cmd_push(&cmd, "-Wall", "-Werror", "-Wextra", "-Wpedantic");
-    nsl_cmd_push(&cmd, "-std=c99");
-    nsl_cmd_push(&cmd, "-Isrc");
-
-    if (nsl_cmd_exec(&cmd) != 0) NSL_DEFER(NSL_ERROR);
-
-defer:
-    nsl_os_remove(NSL_PATH("nsl.o"));
-    return result;
+static void cmd_push_flags(nsl_Cmd *cmd) {
+    nsl_cmd_push(cmd, "-Wall", "-Werror", "-Wextra", "-Wpedantic");
+    nsl_cmd_push(cmd, "-std=c99");
 }
 
-static bool build_tests(void) {
-    nsl_Error result = NSL_NO_ERROR;
+static nsl_Error build_header(nsl_Cmd *cmd) {
+    cmd->len = 0;
 
-    if (NSL_CMD(CC, "-o", "test", "-DNSL_IMPLEMENTATION", "-I.", "tests/main.c") != 0) NSL_DEFER(NSL_ERROR);
-    if (NSL_CMD("test") != 0) NSL_DEFER(NSL_ERROR);
+    nsl_cmd_push(cmd, CC, "-c", "-o", "build/nsl.o", "-DNSL_IMPLEMENTATION", "nsl.h");
+    cmd_push_flags(cmd);
 
-defer:
-    nsl_os_remove(NSL_PATH("test"));
-    return result;
+    if (nsl_cmd_exec(cmd)) return NSL_ERROR;
+
+    return NSL_NO_ERROR;
+}
+
+static nsl_Error build_tests(nsl_Cmd *cmd) {
+    cmd->len = 0;
+
+    nsl_cmd_push(cmd, CC, "-o", "build/test", "-DNSL_IMPLEMENTATION", "-I.", "tests/main.c");
+    cmd_push_flags(cmd);
+    if (nsl_cmd_exec(cmd)) return NSL_ERROR;
+
+    if (NSL_CMD("test")) return NSL_ERROR;
+
+    return NSL_NO_ERROR;
+}
+
+static nsl_Error build_examples(nsl_Cmd *cmd) {
+    cmd->len = 0;
+    NSL_UNUSED(cmd);
+    return NSL_NO_ERROR;
 }
 
 int main(int argc, const char **argv) {
-    if (build_header()) return 1;
-    if (build_tests())  return 2;
+    int result = 0;
+    nsl_Cmd cmd = {0};
 
+    if (nsl_os_mkdir(NSL_PATH("build"), NSL_DEFAULT)) NSL_DEFER(1);
+
+    if (build_header(&cmd))    NSL_DEFER(2);
+    if (build_tests(&cmd))     NSL_DEFER(3);
+    if (build_examples(&cmd))  NSL_DEFER(4);
+
+defer:
+    nsl_list_free(&cmd);
     return 0;
 }
 
