b97599f78bb64321e5d603ec98a33a14965cb925
Code-Nycticebus <loris.kriyonas@gmail.com>
2026-03-02 21:25:21 +0100

    testing windows for windows better

--- build.c
+++ build.c
@@ -1,51 +1,49 @@
 #define NSL_IMPLEMENTATION
 #include "nsl.h"
 
-#ifdef WINDOWS
-#    define _WIN32
-#    define CC "x86_64-w64-mingw32-gcc"
-#endif
-
-#ifndef CC
-#   define CC NSL_COMPILER_NAME
-#endif
-
-static void cmd_push_flags(nsl_Cmd *cmd) {
+static void build_push_flags(nsl_Cmd *cmd) {
     nsl_cmd_push(cmd, "-Wall", "-Werror", "-Wextra", "-Wpedantic");
     nsl_cmd_push(cmd, "-std=c99");
 }
 
-static nsl_Error build_header(nsl_Cmd *cmd) {
-    cmd->len = 0;
-    nsl_cmd_push(cmd, "gcc", "-c", "-o", "build/nsl.o", "-DNSL_IMPLEMENTATION", "nsl.h");
-    cmd_push_flags(cmd);
-
-    if (nsl_cmd_exec(cmd)) return NSL_ERROR;
-
+static nsl_Error build_header_mingw32(nsl_Cmd *cmd) {
     cmd->len = 0;
     nsl_cmd_push(cmd, "x86_64-w64-mingw32-gcc", "-c", "-o", "build/nsl.o", "-DNSL_IMPLEMENTATION", "nsl.h");
-    cmd_push_flags(cmd);
+    build_push_flags(cmd);
 
     if (nsl_cmd_exec(cmd)) return NSL_ERROR;
 
     return NSL_NO_ERROR;
 }
 
-static nsl_Error build_tests(nsl_Cmd *cmd) {
+static nsl_Error build_header_gcc(nsl_Cmd *cmd) {
     cmd->len = 0;
+    nsl_cmd_push(cmd, "gcc", "-c", "-o", "build/nsl.o", "-DNSL_IMPLEMENTATION", "nsl.h");
+    build_push_flags(cmd);
 
-    nsl_cmd_push(cmd, CC, "-o", "build/test", "-DNSL_IMPLEMENTATION", "-I.", "tests/main.c");
-    cmd_push_flags(cmd);
+    if (nsl_cmd_exec(cmd)) return NSL_ERROR;
+
+    return NSL_NO_ERROR;
+}
+
+static nsl_Error build_tests_mingw32(nsl_Cmd *cmd) {
+    cmd->len = 0;
+    nsl_cmd_push(cmd, "x86_64-w64-mingw32-gcc", "-o", "build/test.exe", "-DNSL_IMPLEMENTATION", "-I.", "tests/main.c");
+    build_push_flags(cmd);
+    if (nsl_cmd_exec(cmd)) return NSL_ERROR;
+
+    if (NSL_CMD("wine", "build/test.exe")) return NSL_ERROR;
+    return NSL_NO_ERROR;
+}
+
+static nsl_Error build_tests_gcc(nsl_Cmd *cmd) {
+    cmd->len = 0;
+    nsl_cmd_push(cmd, "gcc", "-o", "build/test", "-DNSL_IMPLEMENTATION", "-I.", "tests/main.c");
+    build_push_flags(cmd);
     if (nsl_cmd_exec(cmd)) return NSL_ERROR;
 
     if (NSL_CMD("build/test")) return NSL_ERROR;
 
-    return NSL_NO_ERROR;
-}
-
-static nsl_Error build_examples(nsl_Cmd *cmd) {
-    cmd->len = 0;
-    NSL_UNUSED(cmd);
     return NSL_NO_ERROR;
 }
 
@@ -55,9 +53,8 @@ int main(int argc, const char **argv) {
 
     if (nsl_os_mkdir(NSL_PATH("build"), .exists_ok=true)) NSL_DEFER(1);
 
-    if (build_header(&cmd))    NSL_DEFER(2);
-    if (build_tests(&cmd))     NSL_DEFER(3);
-    if (build_examples(&cmd))  NSL_DEFER(4);
+    if (build_header_gcc(&cmd)) NSL_DEFER(2);
+    if (build_tests_gcc(&cmd))  NSL_DEFER(3);
 
 defer:
     nsl_list_free(&cmd);
